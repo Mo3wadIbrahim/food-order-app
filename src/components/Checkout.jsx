ee7d38a6de2b06106abaa4e420bfa286d9ff42bd
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-06-30 02:33:08 +0300

    Project Updated

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,6 @@
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
-
 import { use } from "react";
 import Modal from "./UI/Modal.jsx";
 import Button from "./UI/Button.jsx";
@@ -10,11 +9,32 @@ import Input from "./UI/Input.jsx";
 export default function Checkout() {
   const { progress, showCart, hideCheckout } = use(UserProgressContext);
   const { items } = use(CartContext);
-
   const cartTotalPrice = items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0,
   );
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const formData = Object.fromEntries(fd.entries());
+    const response = await fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: [...items],
+          customer: { ...formData },
+        },
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Moawad Ibrahim");
+    }
+  }
   return (
     <Modal
       className={"checkout"}
@@ -22,8 +42,8 @@ export default function Checkout() {
       onClose={progress === "checkout" ? hideCheckout : null}
     >
       <h3>Total Amount: {currencyFormatter.format(cartTotalPrice)}</h3>
-      <form>
-        <Input label="Full Name" type="text" id="full-name" />
+      <form onSubmit={handleSubmit}>
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
@@ -37,7 +57,7 @@ export default function Checkout() {
           <Button textOnly onClick={showCart}>
             Return to Cart
           </Button>
-          <Button>Submit Order</Button>
+          <Button type="submit">Submit Order</Button>
         </p>
       </form>
     </Modal>
